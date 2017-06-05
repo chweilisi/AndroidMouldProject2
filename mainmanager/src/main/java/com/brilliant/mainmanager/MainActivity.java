f8d91e3d8c2c60a24a9443da704f2a77f65250c8
DESKTOP-V2KIE16\Administrator <137148873@qq.com>
2017-06-05 11:35:45 +0800

    持续完善信息

--- mainmanager/src/main/java/com/brilliant/mainmanager/MainActivity.java
+++ mainmanager/src/main/java/com/brilliant/mainmanager/MainActivity.java
@@ -1,13 +1,56 @@
 package com.brilliant.mainmanager;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.baselibrary.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    //##########################  custom variables start ##########################################
+
+
+    //##########################   custom variables end  ##########################################
+
+    //###################### override custom metohds start ########################################
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    public void initPresenter() {
+    }
+
+    @Override
+    public void initView() {
+    }
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+    }
+
+    //######################  override custom metohds end  ########################################
+
+    //######################      custom metohds start     ########################################
+
+    //######################    custom metohds end   ##############################################
+
+    //######################  override methods start ##############################################
+
+    @Override
+    public void onBackPressed() {
+        backPressConform();
+    }
+
+    /**
+     * @Description
+     * @author BrillantZhao
+     * @date 2015-1-16 下午12:56:28
+     */
+    private void backPressConform() {
+        finish();
+    }
+
+    //######################   override methods end  ##############################################
 }
--- testmanager/src/main/java/com/example/testmanager/ChangeAppIconAliasActivity.java
+++ testmanager/src/main/java/com/example/testmanager/ChangeAppIconAliasActivity.java
@@ -1,20 +1,63 @@
 package com.example.testmanager;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import com.basemodule.utils.log.MyLogUtil;
+import com.example.baselibrary.base.BaseActivity;
 import com.example.baselibrary.constant.UIFactory;
 
 /**
  *
  */
-public class ChangeAppIconAliasActivity extends Activity {
+public class ChangeAppIconAliasActivity extends BaseActivity {
+
+    //##########################  custom variables start ##########################################
+
+
+    //##########################   custom variables end  ##########################################
+
+    //###################### override custom metohds start ########################################
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initPresenter() {
+    }
+
+    @Override
+    public void initView() {
         MyLogUtil.i("ChangeAppIconAliasActivity--");
         UIFactory.startTestActivity(this, RESULT_OK);
     }
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+    }
+
+    //######################  override custom metohds end  ########################################
+
+    //######################      custom metohds start     ########################################
+
+    //######################    custom metohds end   ##############################################
+
+    //######################  override methods start ##############################################
+
+    @Override
+    public void onBackPressed() {
+        backPressConform();
+    }
+
+    /**
+     * @Description
+     * @author BrillantZhao
+     * @date 2015-1-16 下午12:56:28
+     */
+    private void backPressConform() {
+        finish();
+    }
+
+    //######################   override methods end  ##############################################
 }
