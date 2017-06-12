4b6b7df35beefeb91e84cbd35af8f8cb13575ea7
DESKTOP-V2KIE16\Administrator <137148873@qq.com>
2017-06-12 10:02:22 +0800

    完善mainFragment的页面加载调用

--- mainmanager/src/main/java/com/brilliant/mainmanager/MainActivity.java
+++ mainmanager/src/main/java/com/brilliant/mainmanager/MainActivity.java
@@ -1,13 +1,27 @@
 package com.brilliant.mainmanager;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
+import com.brilliant.mainlibrary.view.MainPageFragment;
 import com.example.baselibrary.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends BaseActivity {
 
     //##########################  custom variables start ##########################################
 
+    private static final String CURR_INDEX = "currIndex";
+
+    private FragmentManager fragmentManager;
+
+    private static int currIndex = 0;
+
+    private ArrayList<String> fragmentTags;
 
     //##########################   custom variables end  ##########################################
 
@@ -24,15 +38,71 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        fragmentManager = getSupportFragmentManager();
     }
 
     @Override
     public void initData(Bundle savedInstanceState) {
+        fragmentTags = new ArrayList<>(Arrays.asList("HomeFragment"));
+        currIndex = 0;
+        if (savedInstanceState != null) {
+            currIndex = savedInstanceState.getInt(CURR_INDEX);
+            hideSavedFragment();
+        }
+
+        showFragment();
     }
 
     //######################  override custom metohds end  ########################################
 
     //######################      custom metohds start     ########################################
+
+    /**
+     *
+     */
+    private void hideSavedFragment() {
+        Fragment fragment = fragmentManager.findFragmentByTag(fragmentTags.get(currIndex));
+        if (fragment != null) {
+            fragmentManager.beginTransaction().hide(fragment).commit();
+        }
+    }
+
+    /**
+     *
+     */
+    private void showFragment() {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment = fragmentManager.findFragmentByTag(fragmentTags.get(currIndex));
+        if (fragment == null) {
+            fragment = instantFragment(currIndex);
+        }
+        for (int i = 0; i < fragmentTags.size(); i++) {
+            Fragment f = fragmentManager.findFragmentByTag(fragmentTags.get(i));
+            if (f != null && f.isAdded()) {
+                fragmentTransaction.hide(f);
+            }
+        }
+        if (fragment.isAdded()) {
+            fragmentTransaction.show(fragment);
+        } else {
+            fragmentTransaction.add(R.id.fragment_container, fragment, fragmentTags.get(currIndex));
+        }
+        fragmentTransaction.commitAllowingStateLoss();
+        fragmentManager.executePendingTransactions();
+    }
+
+    /**
+     * @param currIndex
+     * @return
+     */
+    private Fragment instantFragment(int currIndex) {
+        switch (currIndex) {
+            case 0:
+                return new MainPageFragment();
+            default:
+                return null;
+        }
+    }
 
     //######################    custom metohds end   ##############################################
 
